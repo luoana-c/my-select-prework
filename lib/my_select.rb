4cd813ad47958737b3108805710f0d64cceabe39
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 08:11:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,10 @@
 def my_select(collection)
   i = 0 
+  new_collection = []
   while i < collection.length 
     item = yield collection[i]
     i += 1 
+    new_collection.push(item)
   end
-  return item
+  return new_collection
 end
