dee97b9caea847e5c3c5af8ec2d50ec27fe201d3
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-04 08:13:59 +0000

    my select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new_collection = []
+  while i < collection.length 
+    item = yield collection[i]
+     
+    if item
+      new_collection.push(collection[i])
+    end
+    i += 1
+  end
+  return new_collection
 end
